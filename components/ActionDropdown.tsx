5dbc5dd765304e454da3ea1614355d11bf7fb282
DENIM <denim@DENIMs-MacBook-Air.local>
2025-04-01 16:39:57 +0530

    Share functionality added

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -27,9 +27,9 @@ import { actionsDropdownItems } from '@/constants';
 import Link from 'next/link';
 import { constructDownloadUrl } from '@/lib/utils';
 import { Button } from "@/components/ui/button";
-import { renameFile } from '@/lib/actions/file.actions';
+import { renameFile, updateFileUsers } from '@/lib/actions/file.actions';
 import { usePathname } from "next/navigation";
-import { FileDetails } from './ActionsModalContent';
+import { FileDetails,ShareInput } from './ActionsModalContent';
   
 const ActionDropdown = ({file} : {file:Models.Document}) => {
     const [isModalOpen , setIsModalOpen] = useState(false);
@@ -37,6 +37,7 @@ const ActionDropdown = ({file} : {file:Models.Document}) => {
     const [action,setAction] = useState<ActionType | null>(null);
     const [name, setName] = useState(file.name);
     const [isLoading, setIsLoading] = useState(false);
+    const [emails, setEmails] = useState<string[]>([]);
     const path = usePathname();
 
     const closeAllModals = () => {
@@ -55,7 +56,7 @@ const ActionDropdown = ({file} : {file:Models.Document}) => {
         const actions = {
           rename: () =>
             renameFile({ fileId: file.$id, name, extension: file.extension, path }),
-          share: () => console.log('share'),
+          share: () => updateFileUsers({ fileId: file.$id, emails, path }),
           delete: () => console.log('share'),
         };
     
@@ -64,6 +65,20 @@ const ActionDropdown = ({file} : {file:Models.Document}) => {
         if (success) closeAllModals();
     
         setIsLoading(false);
+      };
+
+
+      const handleRemoveUser = async (email: string) => {
+        const updatedEmails = emails.filter((e) => e !== email);
+    
+        const success = await updateFileUsers({
+          fileId: file.$id,
+          emails: updatedEmails,
+          path,
+        });
+    
+        if (success) setEmails(updatedEmails);
+        closeAllModals();
       };
 
     const renderDialogContent = () => {
@@ -85,6 +100,13 @@ const ActionDropdown = ({file} : {file:Models.Document}) => {
                 />
               )}
               {value === "details" && <FileDetails file={file} />}
+              {value === "share" && (
+                <ShareInput
+                    file={file}
+                    onInputChange={setEmails}
+                    onRemove={handleRemoveUser}
+                />
+              )}
             </DialogHeader>
             {["rename", "delete", "share"].includes(value) && (
               <DialogFooter className="flex flex-col gap-3 md:flex-row">
